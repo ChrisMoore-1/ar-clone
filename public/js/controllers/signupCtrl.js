320175eff427202ec6f75428e20e80b175b36e16
DividedTruth <christohperdmoore75@gmail.com>
2016-12-07 14:05:52 -0700

    changes made

--- public/js/controllers/signupCtrl.js
+++ public/js/controllers/signupCtrl.js
@@ -619,14 +619,7 @@ $scope.sendForm = function (user) {
   	else return navigator.userAgent;
   }
 
-  /* ////////////////////////////////////////////////////////////////////////////
-  //
-  // Infinite Slider
-  // V 1.0
-  // Alexandra Nantel
-  // Last Update 01/11/2012 10:22
-  //
-  /////////////////////////////////////////////////////////////////////////// */
+  //InfiniteSlider
 
   function InfiniteSlider(wrapper,speed,duration,mode,easing,hover,animation){
   	var _infiniteSlider = this;
@@ -935,5 +928,184 @@ $scope.sendForm = function (user) {
   		});
   	}
   }
+
+
+
+function PNGPreloader(element,frames,width,speed,positioning){
+	var _pngPreloader = this;
+
+	// Params
+	this.animated = false;
+	this.element = element;
+	this.frames = frames;
+	this.currentFrame = 0;
+	this.width = width;
+	this.speed = speed;
+	this.interval;
+	this.delay = (this.frames * this.speed) /2;
+	this.positioning = positioning;
+
+	// Bind
+	$(window).resize(function(){
+		_pngPreloader.positionContent(_pngPreloader);
+	});
+}
+
+/* ////////////////////////////////////////////////////////////////////////////
+//
+// External Functions
+//
+/////////////////////////////////////////////////////////////////////////// */
+
+PNGPreloader.prototype.start = function(goalFrame){
+	var _pngPreloader = this;
+
+	if(!_pngPreloader.animated){
+		_pngPreloader.animated = true;
+		_pngPreloader.interval = setInterval(function(){
+			_pngPreloader.animate(goalFrame,_pngPreloader);
+		} ,_pngPreloader.speed);
+	}
+}
+
+PNGPreloader.prototype.pause = function(){
+	var _pngPreloader = this;
+
+	_pngPreloader.animated = false;
+	clearInterval(_pngPreloader.interval);
+}
+
+PNGPreloader.prototype.reset = function(){
+	var _pngPreloader = this;
+
+	_pngPreloader.animated = false;
+	clearInterval(_pngPreloader.interval);
+	_pngPreloader.currentFrame = 0;
+	$(_pngPreloader.element).css('background-position', '0 0');
+}
+
+/* ////////////////////////////////////////////////////////////////////////////
+//
+// Animate Images
+//
+/////////////////////////////////////////////////////////////////////////// */
+
+PNGPreloader.prototype.animate = function(goalFrame,_pngPreloader){
+	if (typeof goalFrame !== "undefined") {
+	    if(_pngPreloader.currentFrame == goalFrame) _pngPreloader.pause();
+	}
+
+	$(_pngPreloader.element).css('background-position', (- _pngPreloader.currentFrame * _pngPreloader.width)+'px 0');
+
+	_pngPreloader.currentFrame ++;
+	if(_pngPreloader.currentFrame > _pngPreloader.frames) _pngPreloader.currentFrame = 0;
+}
+
+/* ////////////////////////////////////////////////////////////////////////////
+//
+// Position Content
+//
+/////////////////////////////////////////////////////////////////////////// */
+
+PNGPreloader.prototype.positionContent = function(_pngPreloader){
+	if(_pngPreloader.positioning){
+		$(_pngPreloader.element)
+			.css('left',($(_pngPreloader.element).parent().width()/2 - $(_pngPreloader.element).width()/2)+'px')
+			.css('top',($(_pngPreloader.element).parent().height()/2 - $(_pngPreloader.element).height()/2)+'px');
+	}
+}
+
+//PNGPreloaderRows
+
+
+function PNGPreloaderRows(element,frames,width,height,row,speed,positioning){
+	var _pngPreloader = this;
+
+	// Params
+	this.animated = false;
+	this.element = element;
+	this.frames = frames;
+	this.currentFrame = 0;
+	this.width = width;
+	this.height = height;
+	this.row = row;
+	this.speed = speed;
+	this.interval;
+	this.delay = (this.frames * this.speed) /2;
+	this.positioning = positioning;
+
+	// Bind
+	$(window).resize(function(){
+		_pngPreloader.positionContent(_pngPreloader);
+	});
+}
+
+/* ////////////////////////////////////////////////////////////////////////////
+//
+// External Functions
+//
+/////////////////////////////////////////////////////////////////////////// */
+
+PNGPreloaderRows.prototype.start = function(goalFrame){
+	var _pngPreloader = this;
+
+	if(!_pngPreloader.animated){
+		_pngPreloader.animated = true;
+		_pngPreloader.interval = setInterval(function(){
+			_pngPreloader.animate(goalFrame,_pngPreloader);
+		} ,_pngPreloader.speed);
+	}
+}
+
+PNGPreloaderRows.prototype.pause = function(){
+	var _pngPreloader = this;
+
+	_pngPreloader.animated = false;
+	clearInterval(_pngPreloader.interval);
+}
+
+PNGPreloaderRows.prototype.reset = function(){
+	var _pngPreloader = this;
+
+	_pngPreloader.animated = false;
+	clearInterval(_pngPreloader.interval);
+	_pngPreloader.currentFrame = 0;
+	$(_pngPreloader.element).css('background-position', '0 0');
+}
+
+/* ////////////////////////////////////////////////////////////////////////////
+//
+// Animate Images
+//
+/////////////////////////////////////////////////////////////////////////// */
+
+PNGPreloaderRows.prototype.animate = function(goalFrame,_pngPreloader){
+	if (typeof goalFrame !== "undefined") {
+	    if(_pngPreloader.currentFrame == goalFrame) _pngPreloader.pause();
+	}
+
+	var indexX = (_pngPreloader.currentFrame % _pngPreloader.row) * _pngPreloader.width;
+	var indexY = (Math.floor(_pngPreloader.currentFrame / _pngPreloader.row)) * _pngPreloader.height;
+
+	$(_pngPreloader.element).css('background-position', (-indexX)+'px '+(-indexY)+'px');
+
+	_pngPreloader.currentFrame ++;
+	if(_pngPreloader.currentFrame > _pngPreloader.frames) _pngPreloader.currentFrame = 0;
+}
+
+/* ////////////////////////////////////////////////////////////////////////////
+//
+// Position Content
+//
+/////////////////////////////////////////////////////////////////////////// */
+
+PNGPreloaderRows.prototype.positionContent = function(_pngPreloader){
+	if(_pngPreloader.positioning){
+		$(_pngPreloader.element)
+			.css('left',($(window).width()/2 - $(_pngPreloader.element).width()/2)+'px')
+			.css('top',($(window).height()/2 - $(_pngPreloader.element).height()/2)+'px');
+	}
+}
+
 
 });
