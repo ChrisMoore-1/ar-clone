cdfb220c6ab6f34657b5bf69266c539499791355
root <root@ARClone.localdomain>
2016-12-08 21:00:21 +0000

    change port

--- index.js
+++ index.js
@@ -79,6 +79,6 @@ app.post('/contactus', function(req, res, next) {
 })
 
 
-app.listen(8080, function() {
-  console.log('listening on port 8080');
+app.listen(3000, function() {
+  console.log('listening on port 80');
 })
